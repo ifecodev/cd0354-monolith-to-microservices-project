37373b6dcd7f31dd7d29745902ab7cd1bc2a5d66
ifeanyi <ifeanyi@winexviv.com>
2022-11-15 06:25:41 +0100

    working on fix

--- udagram-api-feed/src/aws.ts
+++ udagram-api-feed/src/aws.ts
@@ -5,7 +5,8 @@ import {config} from './config/config';
 // Configure AWS
 const credentials = new AWS.SharedIniFileCredentials({profile: config.aws_profile});
 AWS.config.credentials = credentials;
-
+console.debug(credentials);
+console.debug("credentials");
 export const s3 = new AWS.S3({
   signatureVersion: 'v4',
   region: config.aws_region,
