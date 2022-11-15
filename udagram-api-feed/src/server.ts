dd4b1e544461c88ce99ef36424e6c9dab565f45d
ifeanyi <ifeanyi@winexviv.com>
2022-11-15 01:26:42 +0100

    added github action support

--- udagram-api-feed/src/server.ts
+++ udagram-api-feed/src/server.ts
@@ -11,8 +11,8 @@ import {V0_FEED_MODELS} from './controllers/v0/model.index';
 
 (async () => {
   await sequelize.addModels(V0_FEED_MODELS);
-  console.debug(process.env.POSTGRES_HOST);
-  console.debug("Initialize database connection...");
+  console.debug(process.env.POSTGRES_HOST+" :host");
+  console.debug("Initialize database connections...");
   await sequelize.sync();
 
   const app = express();
